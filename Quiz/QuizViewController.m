57c8f6f6d1f18779d4ffc5cb9a700414a11a982f
Francis Devereux <francis@devrx.org>
2012-10-16 01:06:25 +0100

    Implement "Show Question" and "Show Answer" buttons

--- Quiz/QuizViewController.m
+++ Quiz/QuizViewController.m
@@ -14,16 +14,46 @@
 
 @implementation QuizViewController
 
-- (void)viewDidLoad
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        [questions addObject:@"What is 7 + 7?"];
+        [answers addObject:@"14"];
+        
+        [questions addObject:@"What is the capital of Vermont?"];
+        [answers addObject:@"Montpelier"];
+        
+        [questions addObject:@"From what is cognac made?"];
+        [answers addObject:@"Grapes"];
+    }
+    
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)showQuestion:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    currentQuestionIndex++;
+    
+    if (currentQuestionIndex >= [questions count]) {
+        currentQuestionIndex = 0;
+    }
+    
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+
+    NSLog(@"displaying question: %@", question);
+    [questionField setText:question];
+    
+    [answerField setText:@"???"];
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    [answerField setText:answer];
 }
 
 @end
